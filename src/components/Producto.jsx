890d1de11d807ee7390f50639265337d01b3a758
dfldiego <dfl.diego92@gmail.com>
2020-08-18 20:27:08 -0300

    capturando evento click en boton comprar

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -5,10 +5,21 @@ const Producto = ({ producto }) => {
     //Destructuring del props
     const { id, nombre, precio } = producto;
 
+    //Agregar producto al carrito
+    const seleccionarProducto = (id) => {
+        console.log("comprando.... ", id);
+    }
+
     return (
         <Fragment>
             <h2>{nombre}</h2>
             <p>${precio}</p>
+            <button
+                type="button"
+                onClick={() => seleccionarProducto(id)}
+            >
+                Comprar
+            </button>
         </Fragment>
     );
 }
